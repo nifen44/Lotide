4eb90bd67238a30f5b18f0419a50b9243db14f6b
Wei Huang <huangwei19810803@gmail.com>
2023-05-02 13:43:23 -0600

    add eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,29 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`😄😄😄Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😢😢😢Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function (array1, array2) {
+  const compareResult = true;
+
+  if (array1.length !== array2.length) {
+    compareResult = false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        compareResult = false;
+        break;
+      }
+    }
+  }
+
+  return compareResult;
+};
+
+// TEST
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, "3"]), true);

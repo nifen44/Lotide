a77ea92dc04655c128f002999b1d7b6dfaa7d404
Wei Huang <huangwei19810803@gmail.com>
2023-05-04 11:23:14 -0600

    add map.js

---
+++ map.js
@@ -0,0 +1,35 @@
+const map = function (array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const assertion = function (array1, array2) {
+  const compareResult = true;
+
+  if (array1.length !== array2.length) {
+    compareResult = false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        compareResult = false;
+        break;
+      }
+    }
+  }
+
+  if (compareResult) {
+    console.log(`😄😄😄Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`😢😢😢Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+assertion(
+  map(words, (word) => word[0]),
+  ["g", "c", "t", "m", "t"]
+);
